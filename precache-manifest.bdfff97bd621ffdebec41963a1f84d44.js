a4f0d24ecbbb7bd7406b4846ea333f29fd790c64
vitalypelensky <vitaly.pelensky@redentu.com>
2020-10-21 18:12:57 +0300

    add file to repoz

---
+++ precache-manifest.bdfff97bd621ffdebec41963a1f84d44.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3e1aa9d40b2cba83695efa01ea124bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe116f6c8768ba2865df",
+    "url": "/static/css/main.16caca16.chunk.css"
+  },
+  {
+    "revision": "de1406215e7ddebb6de9",
+    "url": "/static/js/2.134237f2.chunk.js"
+  },
+  {
+    "revision": "1aea28b924530681cdb428e3ae81d6b2",
+    "url": "/static/js/2.134237f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe116f6c8768ba2865df",
+    "url": "/static/js/main.c0526c85.chunk.js"
+  },
+  {
+    "revision": "294982ff4a33cef903c3",
+    "url": "/static/js/runtime-main.a91e34ab.js"
+  },
+  {
+    "revision": "9882d2d7799fb0c187653458187bd805",
+    "url": "/static/media/Prosciutto1.9882d2d7.jpg"
+  },
+  {
+    "revision": "bcfa51b466cc53f77ef9fc686f13faf9",
+    "url": "/static/media/Prosciutto2.bcfa51b4.jpeg"
+  },
+  {
+    "revision": "e66d8faae2b2ac04891e5d57ccc8f927",
+    "url": "/static/media/close.e66d8faa.svg"
+  },
+  {
+    "revision": "75a4eb9651df0e82b71e27e9e60a2859",
+    "url": "/static/media/email.75a4eb96.svg"
+  },
+  {
+    "revision": "25ee0ef3131b51d0b190a7fadef6aadd",
+    "url": "/static/media/exit.25ee0ef3.svg"
+  },
+  {
+    "revision": "2dc917919c432e907f641593c960631e",
+    "url": "/static/media/facebook.2dc91791.svg"
+  },
+  {
+    "revision": "4adfd6a810c3311a40d05343c088bd42",
+    "url": "/static/media/first.4adfd6a8.png"
+  },
+  {
+    "revision": "4219f5d08101d93e8c0e2cff1f118219",
+    "url": "/static/media/instagram.4219f5d0.svg"
+  },
+  {
+    "revision": "8e87c2f8289cb91257626a606ee6a857",
+    "url": "/static/media/menu.8e87c2f8.svg"
+  },
+  {
+    "revision": "74f513e2bf3173d9845003daed40f60b",
+    "url": "/static/media/olx.74f513e2.svg"
+  },
+  {
+    "revision": "0c46e05b83b35fa411c9879fd1274d7c",
+    "url": "/static/media/phone-call.0c46e05b.svg"
+  },
+  {
+    "revision": "01ec4f5dbdc09cf24153ecdf18005637",
+    "url": "/static/media/pork-leg.01ec4f5d.png"
+  },
+  {
+    "revision": "7a0b3a52ef7d99e7a39f729f2140d970",
+    "url": "/static/media/preloaderCircle.7a0b3a52.png"
+  },
+  {
+    "revision": "baeaf0c7f02777839e58eed62cc714c4",
+    "url": "/static/media/preloaderSmall.baeaf0c7.gif"
+  },
+  {
+    "revision": "db6ee511459eafe2dea453735d2b8885",
+    "url": "/static/media/three.db6ee511.png"
+  },
+  {
+    "revision": "974ba8acc2201c3b5ed37bef3fca6458",
+    "url": "/static/media/two.974ba8ac.png"
+  }
+]);
